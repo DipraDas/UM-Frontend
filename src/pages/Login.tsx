5767dfef851549670e0992e7984dba1addbd6304
Dipra <dipradas5940@gmail.com>
2024-07-06 20:44:01 +0600

    [29.1] -:> Login Redirect And Logout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Button } from 'antd';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
 import { useLoginMutation } from '../redux/features/auth/authApi';
 import { useDispatch } from 'react-redux';
-import { setUser } from '../redux/features/auth/authSlice';
+import { setUser, TUser } from '../redux/features/auth/authSlice';
 import { verifyToken } from '../utils/verifyToken';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 const Login = () => {
 
+    const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const { register, handleSubmit } = useForm({
@@ -19,18 +22,25 @@ const Login = () => {
 
     const [login] = useLoginMutation();
 
-    const onSubmit = async (data: { userId: string; password: string; }) => {
-        const userInfo = {
-            id: data.userId,
-            password: data.password
+    const onSubmit = async (data: FieldValues) => {
+        const toastId = toast.loading('Logging in');
+        try {
+            const userInfo = {
+                id: data.userId,
+                password: data.password
+            }
+            const res = await login(userInfo).unwrap();
+            const user = verifyToken(res.data.accessToken) as TUser;
+            dispatch(setUser({
+                user: user,
+                token: res.data.accessToken
+            }))
+            toast.success('Logged in', { id: toastId, duration: 2000 });
+            navigate(`/${user.role}/dashboard`);
+            console.log('data---', res)
+        } catch (err) {
+            toast.error('Something went wrong.', { id: toastId, duration: 2000 })
         }
-        const res = await login(userInfo).unwrap();
-        const user = verifyToken(res.data.accessToken);
-        dispatch(setUser({
-            user: user,
-            token: res.data.accessToken
-        }))
-        console.log('data---', res)
     }
 
     return (
