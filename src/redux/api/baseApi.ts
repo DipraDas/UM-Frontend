274f737663664e6864a054aa9ad0f3597fffcdc1
Dipra <dipradas5940@gmail.com>
2024-07-07 22:14:29 +0600

    [29.6] -:> Handle Expired Refresh Token (Kick Out The User)

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,6 +1,6 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BaseQueryApi, createApi, FetchArgs, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../store';
-import { setUser } from '../features/auth/authSlice';
+import { logout, setUser } from '../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:5000/api/v1',
@@ -14,22 +14,29 @@ const baseQuery = fetchBaseQuery({
     }
 })
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-    const result = baseQuery(args, api, extraOptions);
-
-    const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
-        method: 'GET',
-        credentials: 'include'
-    });
-    const data = await res.json();
-    console.log(data);
-    const user = (api.getState() as RootState).auth.user;
-    api.dispatch(
-        setUser({
-            user,
-            token: data.data.accessToken
-        })
-    )
+const baseQueryWithRefreshToken = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: {}) => {
+    let result = await baseQuery(args, api, extraOptions);
+    console.log('result', result);
+    if (result?.error?.status === 401) {
+        const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
+            method: 'POST',
+            credentials: 'include'
+        });
+        const data = await res.json();
+        console.log(data);
+        if (data?.data?.accessToken) {
+            const user = (api.getState() as RootState).auth.user;
+            api.dispatch(
+                setUser({
+                    user,
+                    token: data.data.accessToken
+                })
+            )
+        } else {
+            api.dispatch(logout());
+        }
+    }
+    result = await baseQuery(args, api, extraOptions);
     return result
 }
 export const baseApi = createApi({
